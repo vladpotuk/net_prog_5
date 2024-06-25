ba35a5008b3e02ecc6543b4020c60076efcd029c
Потук Владислав Олексійович <143324690+vladpotuk@users.noreply.github.com>
2024-06-25 19:11:22 +0300

    update

--- net_prog_5/Program.cs
+++ net_prog_5/Program.cs
@@ -1,2 +1,50 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+public class UDPClient
+{
+    private const int serverPort = 11000;
+    private const string serverIP = "127.0.0.1"; 
+
+    public static void Main()
+    {
+        UdpClient udpClient = new UdpClient();
+
+        try
+        {
+            
+            udpClient.Connect(serverIP, serverPort);
+            Console.WriteLine("Connected to server {0} on port {1}.", serverIP, serverPort);
+
+            while (true)
+            {
+                
+                Console.Write("Enter products (comma separated): ");
+                string productsInput = Console.ReadLine();
+                if (string.IsNullOrEmpty(productsInput))
+                    continue;
+
+             
+                byte[] sendBytes = Encoding.UTF8.GetBytes(productsInput);
+                udpClient.Send(sendBytes, sendBytes.Length);
+
+               
+                IPEndPoint serverEndPoint = new IPEndPoint(IPAddress.Any, 0);
+                byte[] receivedBytes = udpClient.Receive(ref serverEndPoint);
+                string receivedData = Encoding.UTF8.GetString(receivedBytes);
+                Console.WriteLine("Received from server: ");
+                Console.WriteLine(receivedData);
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.ToString());
+        }
+        finally
+        {
+            udpClient.Close();
+        }
+    }
+}
